f6892d8d7385c516622cd44d108c1e42f76aec96
Terry Fadz <157770943+terryatomic@users.noreply.github.com>
2024-02-18 20:12:07 -0600

    Updates to team and removed header third party code

--- header.php
+++ header.php
@@ -20,9 +20,9 @@ $navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
-<script src="https://tools.roobrik.com/widget/widgetjsv2?id=62952a60-ae29-4536-9773-76e782729395&oid=rbWidget_light_left_seniorliving"></script>
+<!-- <script src="https://tools.roobrik.com/widget/widgetjsv2?id=62952a60-ae29-4536-9773-76e782729395&oid=rbWidget_light_left_seniorliving"></script>
 <script src="https://tools.roobrik.com/widget/widgetjsv2?id=402e8a2f-71bc-4be1-94ea-91593c396a55&oid=rbWidget_dark_left_seniorliving"></script>
-<script src="https://tools.roobrik.com/widget/widgetjsv2?id=5cb0f6ee-15b4-432e-8aac-ae4e1caa1794&oid=rbWidget_light_center_seniorliving"></script>
+<script src="https://tools.roobrik.com/widget/widgetjsv2?id=5cb0f6ee-15b4-432e-8aac-ae4e1caa1794&oid=rbWidget_light_center_seniorliving"></script> -->
 </head>
 
 <body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
